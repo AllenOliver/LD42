8033d3441a6414c50ac675fa91eaa6897e4b3bc5
Erik Diep <erik@adjacea.com>
2018-08-10 19:43:23 -0400

    Make play button highlighted by default
    This will cause the main menu to work properly with the default
    EventSystem.

---
+++ LudumDare42/Assets/Menu/Scripts/SelectedFirst.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectedFirst : MonoBehaviour
+{
+    EventSystem eventSystem;
+    bool isSelected;
+
+    void Start()
+    {
+        eventSystem = FindObjectOfType<EventSystem>();
+    }
+
+    void Update()
+    {
+        if (!isSelected)
+        {
+            eventSystem.SetSelectedGameObject(gameObject);
+            isSelected = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        isSelected = false;
+    }
+}
+
