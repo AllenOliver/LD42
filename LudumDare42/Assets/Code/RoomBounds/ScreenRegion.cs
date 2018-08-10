f97475df52eac1f2c2f9b5a4fa9d6e19914c0de6
Allen Oliver <32452527+AllenOliver@users.noreply.github.com>
2018-08-10 18:04:52 -0400

    Added ReadMe

---
+++ LudumDare42/Assets/Code/RoomBounds/ScreenRegion.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenRegion : MonoBehaviour {
+
+    Rigidbody2D _rb2d;
+    BoxCollider2D _collider;
+    [Header("Size for the camera in each room")]
+    public int CameraSizeForRoom;
+    private Player player;
+    //public DoorController door;
+
+    void Awake()
+    {
+        _collider = gameObject.GetComponent<BoxCollider2D>();
+        _rb2d = gameObject.AddComponent<Rigidbody2D>();
+        _rb2d.isKinematic = true;
+        player = FindObjectOfType<Player>();
+    }
+
+    void SetNewCameraBounds()
+    {
+        Camera.main.orthographicSize = CameraSizeForRoom;
+        CameraAlign cam = Camera.main.gameObject.GetComponent<CameraAlign>();
+        cam.SetNewBounds(_collider.bounds);
+        
+    }
+
+    IEnumerator MovePlayer()
+    {
+        player.canMove = false;
+        yield return  new WaitForSeconds(.25f);
+        player.canMove = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Worked");
+            SetNewCameraBounds();
+            //StartCoroutine(MovePlayer());
+           // StartCoroutine(door.SetDoorsActive());
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+           // door.SetDoorsActive();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# LD42
-Tricities Game Dev Group Ludum Dare 42 entry
+# LD 42 - Theme TBA
+## Game Name TBA
+[![N|Solid](http://www.tricitiesgamedev.com/wp-content/uploads/2018/04/SquidTall-6.gif)](http://www.tricitiesgamedev.com/)
+
+Welcome to another Ludum Dare! We will be using unity for this jam like the last, and targeting WebGL! 
+## Let's Win this one!
+
+  - Unity 2018 
+  - ### *Please Make your own branch and version of the level to avoid merge conflicts!*
+
+
+## Installation
+
+#### No installation! 
+Just play the web version on itch.io!
+
+## Developers
+
+Current developers helping with the project! *More will be added later!*
+
+| Contributors | Websites |
+| ------ | ------ |
+| Allen Oliver | http://aribaa.com/ |
+| Gerald Burke | http://www.geraldburke.com/ |
+
+## Todos
+
+ - #### Make the game
+ - #### Host the Game
+ - #### ???
+ - ### Profit!!
+
+License
+----
+
+MIT
+
+
+**Free Software, Hell Yeah!**
+[![N|Solid](http://shobhitsamaria.com/wp-content/uploads/2016/08/MWU-logo-WhiteBG.png)](https://unity3d.com/)
