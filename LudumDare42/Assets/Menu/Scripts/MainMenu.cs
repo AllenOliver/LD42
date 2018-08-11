440c82095c10d24a08f2cd11340eb6fec5894cef
Erik Diep <erik@adjacea.com>
2018-08-11 11:28:36 -0400

    Make play button load first scene and fix button not highlight on start

--- LudumDare42/Assets/Menu/Scripts/MainMenu.cs
+++ LudumDare42/Assets/Menu/Scripts/MainMenu.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Scenes names to load on button press")]
+    [SerializeField] string OnPlayLoad;
     public void OnPlayPressed()
     {
-        Debug.Log("MainMenu.OnPlayPressed()");
+        if (OnPlayLoad != "" && OnPlayLoad != null)
+            SceneManager.LoadScene(OnPlayLoad);
+        else
+        {
+            Debug.LogWarning("MainMenu.OnPlayPressed(): No scene name assigned to play button. Loading default scene with index 0");
+            SceneManager.LoadScene(0);
+        }
     }
 
     public void OnCreditsPressed()
