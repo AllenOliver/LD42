c1307ef05d2301500ab22884f67e5c14f879255c
Allen Oliver <32452527+AllenOliver@users.noreply.github.com>
2018-08-10 20:05:13 -0400

    Added Game Manager, main ui canvas, and file structure

---
+++ LudumDare42/Assets/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Manager for all game files
+/// </summary>
+public class GameManager : MonoBehaviour {
+
+
+    #region Data Variables
+
+    [Header("The memory to fill up")]
+    public int Memory;
+    [Header("How many hits can I take?")]
+    public int MaxPlayerHealth;
+    [Header("How many hits have I taken?")] 
+    public int CurrentPlayerHealth;
+
+
+  
+    Player player;
+
+    #endregion
+
+
+    #region UI Variables
+    public Slider MemoryFill;
+
+    public Text MemoryText;
+    public Text HealthText;
+
+    #endregion
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>();
+        CurrentPlayerHealth = MaxPlayerHealth;
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Sets UI every tick
+    /// </summary>
+    void FixedUpdate()
+    {
+        HealthText.text ="HP: " + MaxPlayerHealth + " / " + CurrentPlayerHealth;
+        MemoryText.text = Memory + " MB";
+        float fillamount = Memory / 100f;
+        MemoryFill.value = Mathf.Clamp01(fillamount);
+    }
+}
